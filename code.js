d2d2aa5f5539560afef496ab5f12099fc083a463
craftyemu <146280572+craftyemu@users.noreply.github.com>
2024-04-18 16:43:30 -0700

    Update code.js and add styling

--- code.js
+++ code.js
@@ -1,22 +1,32 @@
 const container = document.querySelector(".container");
+const create = document.querySelector(".create");
 
-function makeSquares() {
-    var n = prompt("How many squares?");
+function makeGrid() {
+    container.textContent = "";
+    var n = prompt("How many rows and columns? (Must be between 1-100)");
 
-    for (let i = 0; i < n; i++) {
-        var square = document.createElement("div");
-        square.className = "square";
-        container.appendChild(square);
+    if (n > 100 || n < 1) {
+        alert("Please select a number between 1 and 100");
+    } else if (isNaN(n)) {
+        alert("Please select a number between 1 and 100");
+    } else { 
+        for (let i = 0; i < n; i++) {
+        var column = document.createElement("div");
+        column.className = "column"
+        container.appendChild(column);
+
+        for (let i = 0; i < n; i++) {
+            var square = document.createElement("div");
+            square.className = "square";
+            square.addEventListener('mouseover', changeColor);
+            column.appendChild(square);
+            }
+        }
     }
 }
 
-//function getRandomColor() {
-  //  var letters = "0123456789ABCDEF";
-    //var color = "#";
-    //for (var i = 0; i < 6; i++) {
-      //color += letters[Math.floor(Math.random() * 16)];
-    //}
-    //return color;
-  //}
+create.addEventListener("click", makeGrid);
 
-  makeSquares();
+function changeColor(e) {
+    e.target.style.backgroundColor = "black";
+}
